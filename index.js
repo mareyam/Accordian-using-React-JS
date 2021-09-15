c2146fd4e4dc4880098731504487a6394bbd1e35
Maryam Naveed <63284481+mareyam@users.noreply.github.com>
2021-09-15 19:59:45 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import index from './index.css';
+import Accordian from "./Accordian.jsx";
+
+ReactDOM.render(
+        <BrowserRouter> 
+        <Accordian/>
+        </BrowserRouter>
+
+,document.getElementById('root'));
